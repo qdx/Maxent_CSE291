91af4a736698e938934ac242e21e34f5620dc921
qdx <qdxzzz@gmail.com>
2013-03-22 08:57:35 -0700

    all the same buggy version

--- maxent.py
+++ maxent.py
@@ -1,3 +1,4 @@
+import math
 # psudo code
 #@1 Find out how many features are needed,say k features, and what they are
 #@2 Initialize k features to 0
@@ -9,26 +10,127 @@ class Maxent:
 
 	def __init__(self):
 		"model expectation and observed expectation of f_i"
-		self.observed = []
-		self.expected = []
+		self.classes = [0,1,2,3,4,5,6]
+		self.amountOfFeatrues = 35
+		self.features =	['f21','f30','f40','f51','f60','f70','f81','f91','f101','f111','f120','f130','f144','f150','f160','f171','f80','f151','f20','f41','f50','f71','f110','f131','f140','f170','f161','f31','f61','f90','f142','f100','f146','f121','f148','f21','f30','f40','f51','f60','f70','f81','f91','f101','f111','f120','f130','f144','f150','f160','f171','f80','f151','f20','f41','f50','f71','f110','f131','f140','f170','f161','f31','f61','f90','f142','f100','f146','f121','f148']
+		self.observed = dict()
+		for i in self.classes:
+			self.observed[i] = [0 for j in range(self.amountOfFeatrues)]
+
+		self.expected = dict()
+		for i in self.classes:
+			self.expected[i] = [0 for j in range(self.amountOfFeatrues)]
+
+		# Lambdas store the trained lambdas, key of the dict
+		# is the number of lambda, corresponding to the number
+		# of feature functions. That is to say, each feature 
+		# function has its own lambda param.
+		self.Lambdas = dict()
+		for i in self.classes:
+			self.Lambdas[i] = [0 for j in range(self.amountOfFeatrues)] 
 		pass
 
 	def Reset(self):
 		"This method reset the model"
 		pass
 
-	def TrainingInstanceGenerator(self,FilePath):
-		'''This method should serve as a generator that output training
+	def tmpTrainingInstanceParser(self,FilePath):
+		'''This method should serve as a method that output list of training
 		tuples. With the form (class,[fet1,fet2,fet3...])'''
-		pass
+		trainFile = open(FilePath,'r')
+		trainSet = []
+		line = trainFile.readline()
+		while line != '':
+			trainSet.append(line.strip())
+			line = trainFile.readline()
+		return trainSet
+
 
 	def TestInstanceGenerator(self,FilePath):
-		"Just like the TrainingInstanceGenerator, but for test tuples"
+		"Just like the TrainingInstanceParser, but for test tuples"
 		pass
+
+	def tmpFeatureFunctions(self, oneInstance):
+		"This function returns the feature value of one training instance"
+		s = oneInstance.split(' ')
+		result = dict()
+		c = int(s[0][-1]) - 1
+		flist = [0 for i in range(self.amountOfFeatrues)]
+		for i in s[1:]:
+			flist[self.features.index(i)] = 1
+		result[c] = flist
+		return result
 
 	def TrainModel(self):
 		"This method train the model"
-		pass
+		trainSet = self.tmpTrainingInstanceParser("./data/train") 
+
+		# calculate observed
+		for i in trainSet:
+			features = self.tmpFeatureFunctions(i)
+			for y in features:
+				for k in range(self.amountOfFeatrues):
+					self.observed[y][k] += features[y][k]
+		fplist = []
+		for i in self.classes:
+			fplist.append(max(self.observed[i]))
+		fpound = max(fplist)
+
+		for i in self.classes:
+			for k in range(self.amountOfFeatrues):
+				self.observed[i][k] = self.observed[i][k]/fpound
+		#for k in range(len(self.observed)):
+			#self.observed[k] = float(self.observed[k])/len(trainSet)
+		print self.observed
+
+		logFile = open("log.txt",'w')
+		for it in range(50):
+			# i is the ith training instance
+			for i in range(len(trainSet)):
+				features = self.tmpFeatureFunctions(trainSet[i])
+				# y is class label
+				for y in features:
+					middleResult =  [[0 for m in range(len(self.classes))] \
+											for n in range(len(trainSet))]
+					# features is a dict, key is the label of class,
+					# value is a list of feature values{0,1} under that class
+					for k in range(self.amountOfFeatrues):
+						middleResult[i][y] += self.Lambdas[y][k] * features[y][k]
+	
+				z = sum([math.exp(middleResult[i][y]) for y in self.classes])
+				print z
+				for y in features:
+					for k in range(self.amountOfFeatrues):
+						self.expected[y][k] += \
+								features[y][k] * math.exp(middleResult[i][y]) / z
+	
+			for y in self.classes:
+				for k in range(self.amountOfFeatrues):
+					#print "fpound:",fpound
+					#print "ob:",self.observed[k],"exp:",self.expected[k],"k",k
+					#print "ob:",math.log(self.observed[k]),"exp:",math.log(self.expected[k]),"k",k
+					#print "minus log:",(math.log(self.observed[k]) - math.log(self.expected[k]))
+					if self.observed[y][k] == 0:
+						continue
+					delta = (1.0/fpound) * \
+							(math.log(self.observed[y][k]) -\
+							 math.log(self.expected[y][k]))
+					#print delta
+					self.Lambdas[y][k] += delta
+			logFile.write("========="+str(it)+"==========\n")
+			for j in range(self.amountOfFeatrues):
+				for i in self.classes:
+					logFile.write(str(i)+","+self.features[j]+"\t"+str(self.Lambdas[i][j])+"\n")
+		logFile.close()
+
+			
+
+
+
+
+
+							#self.Lambdas[k] * math.exp()
+
 
 	def TestModel(self):
 		"This method test a single model"
@@ -43,4 +145,5 @@ class Maxent:
 		pass
 
 
-
+test = Maxent()
+test.TrainModel()
